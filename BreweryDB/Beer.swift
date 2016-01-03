3432f9f39d64d104db6f01138d42ff09cc2377bc
Jake Welton <jwelton122@googlemail.com>
2016-01-03 16:36:15 +0000

    Add files for Beer class

---
+++ BreweryDB/Beer.swift
@@ -0,0 +1,13 @@
+//
+//  Beer.swift
+//  BreweryDB
+//
+//  Created by Jake Welton on 1/3/16.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import Foundation
+
+class Beer {
+    
+}
