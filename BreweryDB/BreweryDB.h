1cc623623b038bd50a8fae91dabbb0249e4a9385
Jake Welton <jwelton122@googlemail.com>
2016-01-03 15:50:46 +0000

    Add Xcode project files

---
+++ BreweryDB/BreweryDB.h
@@ -0,0 +1,19 @@
+//
+//  BreweryDB.h
+//  BreweryDB
+//
+//  Created by Jake Welton on 1/3/16.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BreweryDB.
+FOUNDATION_EXPORT double BreweryDBVersionNumber;
+
+//! Project version string for BreweryDB.
+FOUNDATION_EXPORT const unsigned char BreweryDBVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BreweryDB/PublicHeader.h>
+
+
