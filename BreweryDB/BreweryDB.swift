a04d8ca0276d86a525b83d473b383bd4e288ac18
Jake Welton <jwelton122@googlemail.com>
2016-01-03 16:34:46 +0000

    Add a public variable for the API key

---
+++ BreweryDB/BreweryDB.swift
@@ -0,0 +1,11 @@
+//
+//  BreweryDB.swift
+//  BreweryDB
+//
+//  Created by Jake Welton on 1/3/16.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import Foundation
+
+public var BreweryDBApiKey: String? = nil
