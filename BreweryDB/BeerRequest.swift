8c45e3d19d9d9fe94ab2e51be59ee12bcbd3ca5d
Jake Welton <jwelton122@googlemail.com>
2016-01-08 21:37:55 +0000

    Add BeerRequest class

---
+++ BreweryDB/BeerRequest.swift
@@ -0,0 +1,13 @@
+//
+//  BeerRequest.swift
+//  BreweryDB
+//
+//  Created by Jake on 08/01/2016.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import Foundation
+
+class BeerRequest {
+    
+}
