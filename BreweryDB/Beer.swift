294f23303879bd5b44f41603889f8ccd3d4b1c1c
Jake Welton <jwelton122@googlemail.com>
2016-01-08 21:36:42 +0000

    Add beer properties

--- BreweryDB/Beer.swift
+++ BreweryDB/Beer.swift
@@ -9,4 +9,25 @@
 import Foundation
 
 class Beer {
+    var identifier: String?
+    var name: String?
+    var description: String?
+    var foodPairings: String?
+    var originalGravity: String?
+    var abv: String?
+    var ibu: String?
+    var glasswareId: String?
+    var glass: String?
+    var styleId: String?
+    var style: String?
+    var isOrganic: Bool?
+    var labels: String?
+    var servingTemperature: String?
+    var servingTemperatureDisplay: String?
+    var status: String?
+    var statusDisplay: String?
+    var availableId: String?
+    var available: String?
+    var beerVariation: String?
+    var year: String?
 }
