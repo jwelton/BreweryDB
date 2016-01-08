10ad6671ed502a6b6856573361c9628f98651a09
Jake Welton <jwelton122@googlemail.com>
2016-01-08 21:38:35 +0000

    Add tests file for BeerRequests

---
+++ BreweryDBTests/BeerRequestTests.swift
@@ -0,0 +1,23 @@
+//
+//  BeerRequestTests.swift
+//  BreweryDB
+//
+//  Created by Jake on 08/01/2016.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import XCTest
+
+class BeerRequestTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+}
