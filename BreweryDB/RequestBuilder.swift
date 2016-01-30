1cc4204c16a317aad26327fdb6e02d3e2022cbd7
Jake Welton <jwelton122@googlemail.com>
2016-01-30 15:31:35 +0000

    Add a request builder class

---
+++ BreweryDB/RequestBuilder.swift
@@ -0,0 +1,13 @@
+//
+//  RequestBuilder.swift
+//  BreweryDB
+//
+//  Created by Jake Welton on 1/30/16.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import Foundation
+
+class RequestBuilder {
+    
+}
