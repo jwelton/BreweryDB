e08fb1a9dd42c68a34a5148f8a39c556a72a425a
Jake Welton <jwelton122@googlemail.com>
2016-01-30 15:57:25 +0000

    Add a tests file for RequestBuilder

---
+++ BreweryDBTests/RequestBuilderTests.swift
@@ -0,0 +1,25 @@
+//
+//  RequestBuilderTests.swift
+//  BreweryDB
+//
+//  Created by Jake Welton on 1/30/16.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import XCTest
+
+class RequestBuilderTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    
+    
+}
