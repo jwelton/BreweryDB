b580dedc429d63d463bd67f04466ac187c355750
Jake Welton <jwelton122@googlemail.com>
2016-04-29 21:07:09 +0100

    Add a Brewery class

---
+++ BreweryDB/Brewery.swift
@@ -0,0 +1,24 @@
+//
+//  Brewery.swift
+//  BreweryDB
+//
+//  Created by Jake Welton on 29/04/2016.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import Foundation
+
+class Brewery {
+    let identifier: String
+    var name: String?
+    var established: Int?
+    var isOrganic: Bool?
+    var description: String?
+    var website: NSURL?
+    var mailingListURL: NSURL?
+    var imagePaths: [String]?
+    
+    init(identifier: String) {
+        self.identifier = identifier
+    }
+}
