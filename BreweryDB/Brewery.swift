37696d1d4ed7fdcf373c8a11855823b705dcc6d3
Jake Welton <jwelton122@googlemail.com>
2016-05-02 01:44:28 +0100

    Add JSONParserEntity conformation for Brewery class

--- BreweryDB/Brewery.swift
+++ BreweryDB/Brewery.swift
@@ -21,3 +21,27 @@ class Brewery {
         self.identifier = identifier
     }
 }
+
+extension Brewery: JSONParserEntity {
+    static func mapJSONToObject(rawBrewery: JSON) -> AnyObject? {
+        guard let identifier = rawBrewery["identifier"] as? String else {
+            return nil
+        }
+        
+        let brewery = Brewery(identifier: identifier)
+        brewery.name = rawBrewery["name"] as? String
+        brewery.established = rawBrewery["established"] as? Int
+        brewery.isOrganic = rawBrewery["isOrganic"] as? String == "Y"
+        brewery.description = rawBrewery["description"] as? String
+        
+        if let websiteURL = rawBrewery["website"] as? String {
+            brewery.website = NSURL(string: websiteURL)
+        }
+        
+        if let mailingListURL = rawBrewery["mailingListURL"] as? String {
+            brewery.website = NSURL(string: mailingListURL)
+        }
+        
+        return brewery
+    }
+}
