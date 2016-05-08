eda960c07ebd23bca7f37e6e7fc1ab423ff44750
Jake Welton <jwelton122@googlemail.com>
2016-05-08 17:05:29 +0100

    Add a Glass class

---
+++ BreweryDB/Glass.swift
@@ -0,0 +1,14 @@
+//
+//  Glass.swift
+//  BreweryDB
+//
+//  Created by Jake Welton on 08/05/2016.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import Foundation
+
+public class Glass {
+    let identifier: String?
+    let name: String?
+}
