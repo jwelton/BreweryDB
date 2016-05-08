c67a0f9029da34508c4afae0e484d0cc926918da
Jake Welton <jwelton122@googlemail.com>
2016-05-08 17:14:16 +0100

    Conform glass to JSONParserEntity

--- BreweryDB/Glass.swift
+++ BreweryDB/Glass.swift
@@ -16,3 +16,16 @@ public class Glass {
         self.identifier = identifier
     }
 }
+
+extension Glass: JSONParserEntity {
+    public static func mapJSONToObject(json: JSON) -> AnyObject? {
+        guard let identifier = json["id"] as? String else {
+            return nil
+        }
+        
+        let glass = Glass(identifier: identifier)
+        glass.name = json["name"] as? String
+        
+        return glass
+    }
+}
