6ea66cb024df8906c22fae4e64c3132b000dc429
Jake Welton <jwelton122@googlemail.com>
2016-05-09 17:57:49 +0100

    Add style class

---
+++ BreweryDB/Style.swift
@@ -0,0 +1,13 @@
+//
+//  Style.swift
+//  BreweryDB
+//
+//  Created by Jake Welton on 5/9/16.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import Foundation
+
+class Style {
+    
+}
