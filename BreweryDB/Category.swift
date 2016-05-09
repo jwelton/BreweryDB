11b490b3e2d1a2517bd2d855f75e421693da45c7
Jake Welton <jwelton122@googlemail.com>
2016-05-09 18:20:11 +0100

    Add Category class

---
+++ BreweryDB/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  BreweryDB
+//
+//  Created by Jake Welton on 5/9/16.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import Foundation
+
+public class Category {
+    
+}
