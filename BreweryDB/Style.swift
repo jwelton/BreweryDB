f1659500f9922f23d240345bf5d005d7a1ee614b
Jake Welton <jwelton122@googlemail.com>
2016-05-09 18:01:00 +0100

    Add style properties

--- BreweryDB/Style.swift
+++ BreweryDB/Style.swift
@@ -9,5 +9,23 @@
 import Foundation
 
 class Style {
+    let identifier: Int
+    var categoryID: Int?
+    var name: String?
+    var shortName: String?
+    var description: String?
+    var ibuMin: Float?
+    var ibuMax: Float?
+    var abvMin: Float?
+    var abvMax: Float?
+    var srmMin: Float?
+    var srmMax: Float?
+    var ogMin: Float?
+    var ogMax: Float?
+    var fgMin: Float?
+    var fgMax: Float?
     
+    init(identifier: Int) {
+        self.identifier = identifier
+    }
 }
