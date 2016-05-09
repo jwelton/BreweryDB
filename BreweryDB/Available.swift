fa98a417d7f461bb4345ebef5aa5f5001b4b386c
Jake Welton <jwelton122@googlemail.com>
2016-05-09 18:30:21 +0100

    Add available class

---
+++ BreweryDB/Available.swift
@@ -0,0 +1,13 @@
+//
+//  Available.swift
+//  BreweryDB
+//
+//  Created by Jake Welton on 5/9/16.
+//  Copyright © 2016 Jake Welton. All rights reserved.
+//
+
+import Foundation
+
+public class Available {
+    
+}
